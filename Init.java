d1ecb8a10a1a5829c27b6c0f753c951212ae0aaf
JASD <86506578+JASDtubz@users.noreply.github.com>
2021-09-27 08:11:23 -0500

    Update Init.java

--- Init.java
+++ Init.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class Init
 {
-    public void init(String s, boolean b0)
+    public void init(String s, boolean b0, boolean b1)
     {
         Main m = new Main();
 
@@ -27,6 +27,7 @@ public class Init
         }
 
         assert bi != null;
+
         try
         {
             h = bi.getHeight();
@@ -34,64 +35,72 @@ public class Init
         }
         catch (Exception ignored) { return; }
 
-        if (h >= 128) d = (double) h / 128;
+        if (h >= 128) { d = (double) h / 128; }
         else
         {
             System.out.println("Image height to small.");
             return;
         }
 
-        if (w >= 128) d_ = (double) w / 128;
+        if (w >= 128) { d_ = (double) w / 128; }
         else
         {
             System.out.println("Image width to small");
             return;
         }
 
-        for (double x = 0; x < w; x += d_) for (double y = 0; y < h; y += d)
+        for (double x = 0; x < w; x += d_)
         {
-            int length = 0;
-            int[][] colors = new int[3][10000];
-
-            for (int z = (int) x; z < x + Math.floor(d_); z++) for (int a = (int) y; a < y + Math.floor(d); a++)
+            for (double y = 0; y < h; y += d)
             {
-                length++;
-                int i = bi.getRGB(z, a);
-                Color c = new Color(i, true);
+                int length = 0;
+                int[][] colors = new int[3][10000];
 
-                int r_ = c.getRed();
-                int g_ = c.getGreen();
-                int b_ = c.getBlue();
+                for (int z = (int) x; z < x + Math.floor(d_); z++)
+                {
+                    for (int a = (int) y; a < y + Math.floor(d); a++)
+                    {
+                        length++;
+                        int i = bi.getRGB(z, a);
+                        Color c = new Color(i, true);
 
-                colors[0][length - 1] = r_;
-                colors[1][length - 1] = g_;
-                colors[2][length - 1] = b_;
+                        int r_ = c.getRed();
+                        int g_ = c.getGreen();
+                        int b_ = c.getBlue();
+
+                        colors[0][length - 1] = r_;
+                        colors[1][length - 1] = g_;
+                        colors[2][length - 1] = b_;
+                    }
+                }
+
+                int rr = 0;
+                int gg = 0;
+                int bb = 0;
+
+                for (int xx = 0; xx < length; xx++)
+                {
+                    rr += colors[0][xx];
+                    gg += colors[1][xx];
+                    bb += colors[2][xx];
+                }
+
+                int ra = rr / length;
+                int ga = gg / length;
+                int ba = bb / length;
+
+                int x_ = (int) Math.ceil(x / d_);
+                int y_ = (int) Math.ceil(y / d);
+
+                MapColor mc;
+
+                if (b0) { mc = new MapColor(ra, ga, ba, true); }
+                else { mc = new MapColor(ra, ga, ba); }
+
+                m.assign(x_, y_, mc);
+
+                System.out.println(x_ + " " + y_ + " " + mc.name);
             }
-
-            int rr = 0;
-            int gg = 0;
-            int bb = 0;
-
-            for (int xx = 0; xx < length; xx++)
-            {
-                rr += colors[0][xx];
-                gg += colors[1][xx];
-                bb += colors[2][xx];
-            }
-
-            int ra = rr / length;
-            int ga = gg / length;
-            int ba = bb / length;
-
-            int x_ = (int) Math.ceil(x / d_);
-            int y_ = (int) Math.ceil(y / d);
-
-            MapColor mc;
-            if (b0) mc = new MapColor(ra, ga, ba, true);
-            else mc = new MapColor(ra, ga, ba);
-            m.assign(x_, y_, mc);
-
-            System.out.println(x_ + " " + y_ + " " + mc.name);
         }
     }
 }
