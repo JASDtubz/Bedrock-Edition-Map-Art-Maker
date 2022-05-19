cb52a6e8fe8564bd102c27ada8b8e7df54496cc0
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-05-19 10:04:25 -0500

    Update script.js

--- html/script.js
+++ html/script.js
@@ -1,7 +1,7 @@
 /*
     Copyright Loshun Ltd. 2022
     Updated Thursday, May 19
-    Version 0.0
+    Version 0.1
 */
 
 function findChunk()
@@ -16,7 +16,7 @@ function findChunk()
     let z0 = z / 16 * 16;
     let z1 = z0 + 127;
     
-    document.getElementById("result").textConent =
+    document.getElementById("result").textContent =
     "Northwest: ( " + x0 + " , " + z0 + " )\nNortheast: ( " + x1 + " , " + z0 +
     " )\nSouthwest: ( " + x0 + " , " + z1 + " )\nSoutheast: ( " + x1 + " , " + z1 + " )";
 }
