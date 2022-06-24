3a370ffdc0f5f8b99c9f1615f1720410873b6a36
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-06-24 11:22:46 -0500

    1.2.0

--- Maven/src/main/java/module-info.java
+++ Maven/src/main/java/module-info.java
@@ -1,10 +1,16 @@
+/*
+    Copyright Loshun Ltd. 2022
+    Updated Friday, June 24
+    Version 1.2.0
+*/
+
 module BedrockMapArt
 {
-    requires javafx.controls;
-    requires javafx.graphics;
-
     requires java.datatransfer;
     requires java.desktop;
+    
+    requires javafx.controls;
+    requires javafx.graphics;
 
     opens main;
 }
